64db465f5890df3ebb9624be8181e706b27e7f65
Jan Müller <janmueller3698@gmail.com>
2021-03-25 15:18:53 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@8668d63242a3898f61d406b9ceb974e6e7f43698 🚀

--- _nuxt/static/1616685202/projects/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/projects", {data:[{projects:[{title:"Under construction",path:"\u002Fprojects\u002Fplaceholder",createdAt:"2021-03-25T15:12:26.801Z"}]}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1616685452/projects/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/projects", {data:[{projects:[{title:"Under construction",path:"\u002Fprojects\u002Fplaceholder",createdAt:"2021-03-25T15:16:35.037Z"}]}],fetch:{},mutations:[]});
