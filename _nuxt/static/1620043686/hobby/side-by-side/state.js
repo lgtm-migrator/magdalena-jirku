f0b5ece630e05d06ea787dc0f5877a8e5a0ead87
Jan Müller <janmueller3698@gmail.com>
2021-05-03 12:09:53 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@7150d1623acd04401dfca8600d62c0a1697ce46f 🚀

---
+++ _nuxt/static/1620043686/hobby/side-by-side/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620043686",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fhobby\u002Fside-by-side",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"6544f269"}},globalRefs:{}}}(null,true));
