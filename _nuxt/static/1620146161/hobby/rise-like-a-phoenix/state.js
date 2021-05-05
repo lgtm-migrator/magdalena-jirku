7dc3bfa8a63c8eae5dd5ace3438e348a4eb5add4
Jan Müller <janmueller3698@gmail.com>
2021-05-05 19:09:31 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@307fd33d75ade2b089ad4bab1636264d39d28c8f 🚀

--- _nuxt/static/1620146161/hobby/rise-like-a-phoenix/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620146161",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fhobby\u002Frise-like-a-phoenix",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"460b4167"}},globalRefs:{}}}(null,true));
