63bfcbe2ef82dd1509b890ab21c418a73ab96df0
Jan Müller <janmueller3698@gmail.com>
2021-05-08 22:39:52 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@7dba07dabc05312837f7e30c17baf733d59d7aed 🚀

---
+++ _nuxt/static/1620513506/projects/jm/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620513506",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fprojects\u002Fjm",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c3b0bc61"}},globalRefs:{}}}(null,true));
---
+++ _nuxt/static/1620513506/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620513506",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c3b0bc61"}},globalRefs:{}}}(null,true,"\u002F"));
