b4c9a771c0bdf963928f9b75497324b9b2207f3e
Jan Müller <janmueller3698@gmail.com>
2021-06-03 17:11:57 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@d0044a7e67d9ea88252e867a7e16e0a4eab3025c 🚀

---
+++ _nuxt/static/1622740238/projects/letter-cards/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622740238",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fprojects\u002Fletter-cards",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"72d45410"}},globalRefs:{}}}(null,true));
