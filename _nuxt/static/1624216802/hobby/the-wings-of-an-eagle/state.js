1b51251592483964677b22b065c53e67ca099608
Jan Müller <janmueller3698@gmail.com>
2021-06-22 21:10:22 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@3e973da7c3060679c69fea93070c6da45e9c0769 🚀

--- _nuxt/static/1624216802/hobby/the-wings-of-an-eagle/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624216802",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fhobby\u002Fthe-wings-of-an-eagle",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"db0f68cb"}},globalRefs:{}}}(null,true));
--- _nuxt/static/1624216802/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624216802",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"db0f68cb"}},globalRefs:{}}}(null,true,"\u002F"));
