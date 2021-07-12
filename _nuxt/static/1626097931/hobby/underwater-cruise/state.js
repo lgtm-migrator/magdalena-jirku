b4a583eca7953b911f501f14e9318dca5712a350
Jan Müller <janmueller3698@gmail.com>
2021-07-12 13:54:38 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@8988b4d4c97422481da24de0d72829b04eaeb2ac 🚀

---
+++ _nuxt/static/1626097931/hobby/underwater-cruise/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626097931",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fhobby\u002Funderwater-cruise",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0990ed9f"}},globalRefs:{}}}(null,true));
