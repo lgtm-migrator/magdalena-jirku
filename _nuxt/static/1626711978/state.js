2da516c8d75ee102dec0e31f3cd8d36cb87060b9
Jan Müller <janmueller3698@gmail.com>
2021-07-19 16:27:32 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@2a1ff6dec394aa71ecff561f0c37e3c3a62fa472 🚀

---
+++ _nuxt/static/1626711978/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626711978",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"ac17e1b7"}},globalRefs:{}}}(null,true,"\u002F"));
