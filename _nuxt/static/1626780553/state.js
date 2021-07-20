12cf0a619e9710ee1eda9e60474ec623fb6e4f46
Jan Müller <janmueller3698@gmail.com>
2021-07-20 16:53:24 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@f97b3afe3b1c47ab69c59e940e5667091bac655f 🚀

--- _nuxt/static/1626780553/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626780553",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d2793ead"}},globalRefs:{}}}(null,true,"\u002F"));
---
+++ _nuxt/static/1626799925/projects/harmony/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626799925",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fprojects\u002Fharmony",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"18424e65"}},globalRefs:{}}}(null,true));
