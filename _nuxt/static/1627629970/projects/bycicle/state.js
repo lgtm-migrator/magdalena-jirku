119f450cd582edf9391141bb19115896719adec0
Jan Müller <janmueller3698@gmail.com>
2021-07-30 07:28:19 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@be72c77f5fe2ad8fc619a15d478f3ab877fc17a5 🚀

---
+++ _nuxt/static/1627629970/projects/bycicle/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1627629970",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fprojects\u002Fbycicle",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"5a1c8e93"}},globalRefs:{}}}(null,true));
