6d26f5605f607b83e16b858eb075407f25c579c6
Jan Müller <janmueller3698@gmail.com>
2021-08-16 11:30:17 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@f7a51be17b898d5dfa4191633c3a587f01c2f311 🚀

---
+++ _nuxt/static/1629113272/hobby/meet-the-artist/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629113272",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fhobby\u002Fmeet-the-artist",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d4010837"}},globalRefs:{}}}(null,true));
