67bf112a159298056816c0e686b1b15d63d86b16
Jan Müller <janmueller3698@gmail.com>
2021-08-16 15:31:44 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@38a479dd17ba9eb2330be5fb1dfeb8f011a4de24 🚀

---
+++ _nuxt/static/1629127784/hobby/its-good-to-be-king/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629127784",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fhobby\u002Fits-good-to-be-king",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b3119ba6"}},globalRefs:{}}}(null,true));
