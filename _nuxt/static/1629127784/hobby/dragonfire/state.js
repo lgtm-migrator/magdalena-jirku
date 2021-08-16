f40a3dd30268eded1301e7c22e37588930d17a76
Jan Müller <janmueller3698@gmail.com>
2021-08-16 17:04:15 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@053adc46894dd86225ef6b0bce2f85f78e8cc731 🚀

--- _nuxt/static/1629127784/hobby/dragonfire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1629127784",layout:"default",error:a,state:{useDarkTheme:b,breadcrumbs:[],i18n:{routeParams:{}}},serverRendered:b,routePath:"\u002Fhobby\u002Fdragonfire",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b3119ba6"}},globalRefs:{}}}(null,true));
