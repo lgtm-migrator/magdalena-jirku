56b8a0bd313c8bb86eef93a5aa3cba9f58b31e7a
Jan Müller <janmueller3698@gmail.com>
2022-10-25 08:31:04 +0000

    Deploying to gh-pages from @ DerYeger/magdalena-jirku@da5af0b52520c0c39e4b9c7eb677c51ef99dc606 🚀

--- assets/slide-in.880839c5.js
+++
@@ -1,1 +0,0 @@
-import{u as t}from"./Gallery.vue_vue_type_script_setup_true_lang.9eb867bc.js";import{w as n}from"./app.c32ad8bd.js";function d(i){n(i,s=>s==null?void 0:s.classList.add("slide-in")),t(i,s=>{s.forEach(r=>{r.isIntersecting&&r.target.classList.add("slide-in-visible")})})}export{d as u};
